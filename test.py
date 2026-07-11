18da632c4f212a79622c56f02339d9f6c5715866
sangeetapr26 <sangeetaprasad057@gmail.com>
2026-07-11 18:49:52 +0530

    Update changes

--- test.py
+++ test.py
@@ -1,39 +1,21 @@
 import os
-
+import streamlit as st
 from dotenv import load_dotenv
 from groq import Groq
 
 
 load_dotenv()
 
+# Try local .env first
 api_key = os.getenv("GROQ_API_KEY")
 
+# If deployed on Streamlit Cloud, use Streamlit Secrets
 if not api_key:
-    raise ValueError(
-        "GROQ_API_KEY not found in .env file"
-    )
+    try:
+        api_key = st.secrets["GROQ_API_KEY"]
+    except Exception:
+        api_key = None
 
-
-client = Groq(
-    api_key=api_key
-)
-
-
-response = client.chat.completions.create(
-
-    model="llama-3.3-70b-versatile",
-
-    messages=[
-        {
-            "role": "user",
-            "content": "Wish the user."
-        }
-    ]
-)
-
-
-print("Groq API is working!")
-
-print(
-    response.choices[0].message.content
-)
+if not api_key:
+    st.error("GROQ_API_KEY was not found.")
+    st.stop()
